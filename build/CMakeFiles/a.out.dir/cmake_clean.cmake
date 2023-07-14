451dc40ee6c5347c4fe7b13d7da1c320968c67c6
Lucas-Rezende <lucasmomede@yahoo.com.br>
2023-07-14 16:41:33 -0300

    Swapping Makefile for CMake and testing FTXUI

---
+++ build/CMakeFiles/a.out.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a.out.dir/main.cpp.o"
+  "CMakeFiles/a.out.dir/main.cpp.o.d"
+  "a.out"
+  "a.out.pdb"
+  "obj/getcoordinates.o"
+  "obj/gettime.o"
+  "obj/hourlydatafilter.o"
+  "obj/weatherdatafetcher.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -2,11 +2,16 @@
 #include <fstream>
 #include <string>
 #include <cstdlib>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
 
 #include "include/weatherdatafetcher.hpp"
 #include "include/getcoordinates.hpp"
 #include "include/gettime.hpp"
 #include "include/hourlydatafilter.hpp"
+
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
 
 int main()
 {
@@ -37,17 +42,33 @@ int main()
     if (gt.Hour() >= 10)
     {
         temptest = date + "T" + std::to_string(gt.Hour()) + ":00";
-    } else
+    }
+    else
     {
         temptest = date + "T0" + std::to_string(gt.Hour()) + ":00";
     }
 
-    std::cout << "Temptest:" << temptest << std::endl;
-
-    std::cout << "Temperatura nesse momento: " << hdf.getTemperatureDataAtTime(temptest) << std::endl;
+    std::string tn = hdf.getTemperatureDataAtTime("2023-07-14T09:00");
     std::cout << "Umidade nesse momento: " << hdf.getRelativeHumidityDataAtTime(temptest) << std::endl;
     std::cout << "Chance de precipitação nesse momento: " << hdf.getPrecipitationProbabilityDataAtTime(temptest) << std::endl;
     std::cout << "Weathercode nesse momento: " << hdf.getWeatherCodeDataAtTime(temptest) << std::endl;
+
+    using namespace ftxui;
+
+    // Define the document
+    Element document =
+        hbox({
+            text(temptest) | border,
+            text("middle") | border | flex,
+            text(tn  + "°C") | border,
+        });
+
+    auto screen = Screen::Create(
+        Dimension::Full(),       // Width
+        Dimension::Fit(document) // Height
+    );
+    Render(screen, document);
+    screen.Print();
 
     return 0;
 }
